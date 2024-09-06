7a0668f0b777574c03f3fa4a2bad8df18cabd6fd
supportlaver <ak4686@naver.com>
2024-09-06 21:12:14 +0900

    hotfix: logout 로직 수정

--- src/main/java/com/example/ear/controller/MemberController.java
+++ src/main/java/com/example/ear/controller/MemberController.java
@@ -7,7 +7,9 @@ import com.example.ear.dto.response.JoinResponseDto;
 import com.example.ear.dto.response.LoginResponseDto;
 import com.example.ear.global.SessionConst;
 import com.example.ear.service.MemberService;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -45,10 +47,17 @@ public class MemberController {
     }
 
     @PostMapping("/logout")
-    public ResponseEntity<String> logout(HttpServletRequest request) {
+    public ResponseEntity<String> logout(HttpServletRequest request , HttpServletResponse response) {
         HttpSession session = request.getSession(false);
         if (session != null) {
             session.invalidate();
+            // 쿠키 무효화
+            Cookie cookie = new Cookie("JSESSIONID", null); // 'JSESSIONID'는 세션 쿠키 이름
+            cookie.setPath("/"); // 애플리케이션 루트 경로에 설정
+            cookie.setHttpOnly(true);
+            cookie.setMaxAge(0); // 쿠키 만료 시간 0으로 설정하여 쿠키를 무효화
+            response.addCookie(cookie);
+
             return ResponseEntity.ok("Logged out successfully.");
         } else {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("No active session found.");
